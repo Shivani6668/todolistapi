e2d90f152c50511ea2e7b327202bce9a62f7c5e6
Shivani6668 <shivaniparmar316@gmail.com>
2023-09-23 22:38:27 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,48 +1,12 @@
-// import React,{useEffect,useState} from 'react'
-// import './App.css'
-// import TodoContainer from './Components/TodoContainer'
-// import {Coloring} from ""
 
-// function App() {
-// const [json,setJson] = useState([])
-// const[loading,setLoading] = useState(false)
-
-// useEffect(()=>{
-//   fetch("https://jsonplaceholder.typicode.com/users/1/todos")
-//   .then((response)=> response.json())
-//   .then((json)=>{
-//     setTimeout(()=>{
-//       setJson(json)
-//       setLoading(true);
-//     }, 1000);
-
-//   });
-// });
-
-//   return (
-//     <>
-//     {loading ? (<TodoContainer jsonTodos={json}/>
-//     ):(
-//       <Coloring 
-//       visible={true}
-//       height="80"
-//       width="80"
-//       ariaLabel = "blocks-wrapper"
-//       colors={['#e15b64','#f47e60','#f8b26a','#abbd81','#849b87']}
-//       />
-//     )}
-// </>
-//     );
-// }
-// export default App;
 
 import React from 'react'
-import Home from './component/Home'
+
 import Task from './component/Task';
-import Todo from './component/Todo';
+
 function App() {
   return (  
-    // <Home></Home>
+ 
  <Task></Task>
 );
 }
--- src/component/Task.jsx
+++ src/component/Task.jsx
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { AiFillCheckCircle } from "react-icons/ai";
-import './Task.css'; // Replace 'your-css-file.css' with the path to your CSS file.
+import "./Task.css"; // Replace 'your-css-file.css' with the path to your CSS file.
 
 function Task() {
-  const [check, setCheck] = useState("all"); // Initialize check state with "all"
+  const [check, setCheck] = useState("all");
   const [todos, setTodos] = useState([]);
-  const data = { title: "", completed: false };
-  let [edit, setEdit] = useState("");
-  const [inputdata, setInputdata] = useState(data);
+  const [newTask, setNewTask] = useState(""); // State for adding a new task
+  const [editTask, setEditTask] = useState(null); // State for editing a task
 
-  // Fetch task
+  // Fetch tasks
   const apiUrl = "https://jsonplaceholder.typicode.com/users/1/todos";
   useEffect(() => {
     axios
       .get(apiUrl)
       .then((response) => {
-        // Capitalize the titles before setting the state
         const capitalizedTodos = response.data.map((task) => ({
           ...task,
           title: task.title.charAt(0).toUpperCase() + task.title.slice(1),
@@ -28,57 +26,56 @@ function Task() {
       });
   }, []);
 
-  // Add Task
-  const handlerdata = (e) => {
-    setInputdata({ ...inputdata, [e.target.name]: e.target.value });
+  // Handle input change for adding a new task
+  const handleNewTaskChange = (e) => {
+    setNewTask(e.target.value);
   };
 
-  const handlersubmit = async (e) => {
-    e.preventDefault();
-    if (inputdata.title.trim() !== "") { // Check if the title is not empty or only whitespace
-      setTodos([inputdata, ...todos]);
-      setInputdata(data); // Clear the input field after adding a task
-    }
-  };
-
-  // Edit Task
-  const handleEdit = (id) => {
-    const taskToEdit = todos.find((task) => task.id === id);
-    if (taskToEdit) {
-      setInputdata({ ...taskToEdit });
-      setEdit(id);
-    }
-  };
-
-  const handleSave = (id) => {
-    setTodos((prev) =>
-      prev.map((task) => {
-        if (task.id === id) {
-          return inputdata;
-        } else {
-          return task;
-        }
-      })
+  // Handle input change for editing a task
+  const handleEditTaskChange = (e) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((task) =>
+        task.id === editTask ? { ...task, title: e.target.value } : task
+      )
     );
-    setInputdata(data);
-    setEdit(""); // Clear the edit mode
   };
 
-  // Delete
-  const handlerDelete = async (id) => {
-    setTodos(todos.filter((p) => p.id !== id));
+  // Add a new task
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      setTodos((prevTodos) => [
+        {
+          id: Date.now(),
+          title: newTask,
+          completed: false,
+        },
+        ...prevTodos, // Prepend the new task to the existing tasks
+      ]);
+      setNewTask(""); // Clear the new task input
+    }
+  };
+
+  // Edit a task
+  const handleEdit = (id) => {
+    setEditTask(id);
+  };
+
+  // Save edited task
+  const handleSave = () => {
+    setEditTask(null); // Clear the edit mode
+  };
+
+  // Delete a task
+  const handleDelete = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((task) => task.id !== id));
   };
 
   // Toggle completion status
   const handleToggleComplete = (id) => {
-    setTodos((prev) =>
-      prev.map((task) => {
-        if (task.id === id) {
-          return { ...task, completed: !task.completed };
-        } else {
-          return task;
-        }
-      })
+    setTodos((prevTodos) =>
+      prevTodos.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
     );
   };
 
@@ -92,105 +89,113 @@ function Task() {
 
   return (
     <>
-      <div className="flex justify-center pt-20">
-        <div className="flow-root bg-gray-200">
-          <div className="">
-            <h2 className="text-4xl font-extrabold flex justify-center mb-8">
-              Create Your Todo List
-            </h2>
-          </div>
-          <form className="flex justify-center " action="#" method="POST">
-            <div className="mt-2">
-              <div className="flex items-center space-x-4">
-                <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate dark:text-black">
-                    <input
-                      type="text"
-                      onChange={handlerdata}
-                      value={inputdata.title}
-                      name="title"
-                      className="block w-full rounded-md border-0 py-2 w-200 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </p>
-                </div>
-                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-black">
-                  <button
-                    onClick={handlersubmit}
-                    type="button"
-                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                  >
-                    Add Task
-                  </button>
-                </div>
+      <div className="flow-root bg-gray-200">
+        <div className="">
+          <h2 className="text-4xl font-extrabold flex justify-center mb-8">
+            Create Your Todo List
+          </h2>
+        </div>
+        <form className="flex justify-center " action="#" method="POST">
+          <div className="mt-2">
+            <div className="flex items-center space-x-4">
+              <div className="flex-1 min-w-0">
+                <p className="text-sm font-medium text-gray-900 truncate dark:text-black">
+                  <input
+                    type="text"
+                    onChange={handleNewTaskChange}
+                    value={newTask}
+                    name="title"
+                    className="block w-full rounded-md border-0 py-2 w-200 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </p>
               </div>
-
-              <div className="flex flex-wrap">
-                <input
-                  onChange={() => setCheck("false")}
-                  type="radio"
-                  id="pending"
-                  name="task"
-                  value="false"
-                  className="w-4 h-4 bg-gray-100 border-gray-300"
-                />
-                <label
-                  htmlFor="pending"
-                  className="ml-2 text-sm font-medium text-gray-900"
+              <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-black">
+                <button
+                  onClick={handleAddTask}
+                  type="button"
+                  className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 >
-                  Pending Task
-                </label>
-
-                <input
-                  onChange={() => setCheck("all")}
-                  type="radio"
-                  id="all"
-                  name="task"
-                  value="all"
-                  className="w-4 h-4 bg-gray-100 border-gray-300"
-                />
-                <label htmlFor="all" className="ml-2 text-sm font-medium text-gray-900">
-                  All Task
-                </label>
-
-                <input
-                  onChange={() => setCheck("true")}
-                  type="radio"
-                  id="completed"
-                  name="task"
-                  value="true"
-                  className="w-4 h-4 bg-gray-100 border-gray-300"
-                />
-                <label
-                  htmlFor="completed"
-                  className="ml-2 text-sm font-medium text-gray-900"
-                >
-                  Completed Task
-                </label>
+                  Add Task
+                </button>
               </div>
             </div>
-          </form>
-        </div>
 
-        <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+            <div className="flex flex-wrap pt-3 ">
+              <input
+                onChange={() => setCheck("false")}
+                type="radio"
+                id="pending"
+                name="task"
+                value="false"
+                className="w-4 h-4 bg-gray-100 border-gray-300"
+              />
+              <label
+                htmlFor="pending"
+                className="ml-2 text-sm font-medium text-gray-900"
+              >
+                Pending Task
+              </label>
+
+              <input
+                onChange={() => setCheck("all")}
+                type="radio"
+                id="all"
+                name="task"
+                value="all"
+                className="w-4 h-4 bg-gray-100 border-gray-300"
+              />
+              <label
+                htmlFor="all"
+                className="ml-2 text-sm font-medium text-gray-900"
+              >
+                All Task
+              </label>
+
+              <input
+                onChange={() => setCheck("true")}
+                type="radio"
+                id="completed"
+                name="task"
+                value="true"
+                className="w-4 h-4 bg-gray-100 border-gray-300"
+              />
+              <label
+                htmlFor="completed"
+                className="ml-2 text-sm font-medium text-gray-900"
+              >
+                Completed Task
+              </label>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div className="flex justify-center pt-10">
+        <ul
+          role="list"
+          className="divide-y divide-gray-200 dark:divide-gray-700 bg-slate-200"
+        >
+          <h6 className="pt-2 text-4xl flex justify-center mb-8">
+            List of Task
+          </h6>
           {filteredTodos.map((data) => (
-            <li className="py-3 sm:py-4">
+            <li className="py-3 sm:py-4" key={data.id}>
               <div className="flex items-center space-x-4">
                 <div className="flex-shrink-0">
                   <AiFillCheckCircle
                     className={`w-8 h-8 rounded-full ${
-                      data.completed ? 'green-icon' : 'red-icon'
+                      data.completed ? "green-icon" : "red-icon"
                     }`}
                     onClick={() => handleToggleComplete(data.id)}
                   />
                 </div>
 
                 <div className="flex-1 min-w-0">
-                  {edit === data.id ? (
+                  {editTask === data.id ? (
                     <input
                       type="text"
-                      onChange={handlerdata}
-                      value={inputdata.title}
-                      name="title"
+                      onChange={handleEditTaskChange}
+                      value={data.title}
                       className="block w-full rounded-md border-0 py-2 w-200 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   ) : (
@@ -211,9 +216,9 @@ function Task() {
                     </p>
                   )}
 
-                  {edit === data.id ? (
+                  {editTask === data.id ? (
                     <button
-                      onClick={() => handleSave(data.id)}
+                      onClick={handleSave}
                       type="button"
                       className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                     >
@@ -230,7 +235,7 @@ function Task() {
                       </button>
 
                       <button
-                        onClick={() => handlerDelete(data.id)}
+                        onClick={() => handleDelete(data.id)}
                         type="button"
                         className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                       >
